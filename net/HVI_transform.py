829e9afe6eaea15aaa3759be0d90636839e673f3
csm <2402227288@qq.com>
2024-12-11 09:53:56 +0800

    注释修改

--- net/HVI_transform.py
+++ net/HVI_transform.py
@@ -12,57 +12,57 @@ class RGB_HVI(nn.Module):
         self.alpha = 1.0
         self.this_k = 0
         
+        #img维度为BCHW
     def HVIT(self, img):
         eps = 1e-8
         device = img.device
         dtypes = img.dtype
-        hue = torch.Tensor(img.shape[0], img.shape[2], img.shape[3]).to(device).to(dtypes)
-        value = img.max(1)[0].to(dtypes)
-        img_min = img.min(1)[0].to(dtypes)
-        hue[img[:,2]==value] = 4.0 + ( (img[:,0]-img[:,1]) / (value - img_min + eps)) [img[:,2]==value]
+        hue = torch.Tensor(img.shape[0], img.shape[2], img.shape[3]).to(device).to(dtypes) # B H W
+        value = img.max(1)[0].to(dtypes) ## max(1)会返回值+索引，value维度为BHW
+        img_min = img.min(1)[0].to(dtypes)# BHW
+        hue[img[:,2]==value] = 4.0 + ( (img[:,0]-img[:,1]) / (value - img_min + eps)) [img[:,2]==value] 
         hue[img[:,1]==value] = 2.0 + ( (img[:,2]-img[:,0]) / (value - img_min + eps)) [img[:,1]==value]
         hue[img[:,0]==value] = (0.0 + ((img[:,1]-img[:,2]) / (value - img_min + eps)) [img[:,0]==value]) % 6
 
         hue[img.min(1)[0]==value] = 0.0
-        hue = hue/6.0
+        hue = hue/6.0 # 这里算的hue就是Py？？？很奇怪
 
-        saturation = (value - img_min ) / (value + eps )
+        saturation = (value - img_min ) / (value + eps ) #BHW
         saturation[value==0] = 0
-
         hue = hue.unsqueeze(1)
-        saturation = saturation.unsqueeze(1)
-        value = value.unsqueeze(1)
+        saturation = saturation.unsqueeze(1) # B1HW
+        value = value.unsqueeze(1)#B1HW
         
         k = self.density_k
         self.this_k = k.item()
         
-        color_sensitive = ((value * 0.5 * pi).sin() + eps).pow(k)
-        ch = (2.0 * pi * hue).cos()
-        cv = (2.0 * pi * hue).sin()
-        H = color_sensitive * saturation * ch
-        V = color_sensitive * saturation * cv
-        I = value
-        xyz = torch.cat([H, V, I],dim=1)
+        color_sensitive = ((value * 0.5 * pi).sin() + eps).pow(k) # CK B1HW
+        cx = (2.0 * pi * hue).cos() #h
+        cy = (2.0 * pi * hue).sin() #v 
+        X = color_sensitive * saturation * cx #H  #B1HW
+        Y = color_sensitive * saturation * cy #V  #B1HW
+        Z = value #I
+        xyz = torch.cat([X, Y, Z],dim=1) #B3HW
         return xyz
     
-    def PHVIT(self, img):
+    def PHVIT(self, img): # HVI转HSV转RGB
         eps = 1e-8
         H,V,I = img[:,0,:,:],img[:,1,:,:],img[:,2,:,:]
         
-        # clip
-        H = torch.clamp(H,-1,1)
-        V = torch.clamp(V,-1,1)
-        I = torch.clamp(I,0,1)
+        # clip,限制张量的范围
+        H = torch.clamp(H,-1,1) #-1到1
+        V = torch.clamp(V,-1,1) #-1到1
+        I = torch.clamp(I,0,1)  # 0到1
         
-        v = I
+        v = I 
         k = self.this_k
-        color_sensitive = ((v * 0.5 * pi).sin() + eps).pow(k)
+        color_sensitive = ((v * 0.5 * pi).sin() + eps).pow(k)# CK
         H = (H) / (color_sensitive + eps)
         V = (V) / (color_sensitive + eps)
         H = torch.clamp(H,-1,1)
         V = torch.clamp(V,-1,1)
-        h = torch.atan2(V + eps,H + eps) / (2*pi)
-        h = h%1
+        h = torch.atan2(V + eps,H + eps) / (2*pi)## 反正切atan2，[-pi,pi]归一化到[-1,1]
+        h = h%1 ##限制再0-1之间
         s = torch.sqrt(H**2 + V**2 + eps)
         
         if self.gated:
@@ -71,7 +71,7 @@ class RGB_HVI(nn.Module):
         s = torch.clamp(s,0,1)
         v = torch.clamp(v,0,1)
         
-        r = torch.zeros_like(h)
+        r = torch.zeros_like(h) #和h维度一致全0的张量
         g = torch.zeros_like(h)
         b = torch.zeros_like(h)
         
